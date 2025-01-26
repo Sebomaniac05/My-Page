7e0f1d5627c98da9b1ac4cd4efff833f8b3f063b
Sebastian Johannessen <sebastianjohannessen@Sebastians-MacBook-Pro.local>
2025-01-25 17:37:06 -0800

    testing for deployment

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('dist'))
+
+// MongoDB connection
+mongoose.connect('', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB connected...');
+}).catch(err => {
+  console.log('MongoDB connection error: ', err);
+});
+
+// Define Inquiry Schema and Model
+const inquirySchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String,
+}, { timestamps: true });
+
+const Inquiry = mongoose.model('Inquiry', inquirySchema);
+
+// POST endpoint for inquiries
+app.post('/api/inquiries', async (req, res) => {
+    console.log("HIIII")
+  try {
+    const { name, email, message } = req.body;
+    const newInquiry = new Inquiry({ name, email, message });
+    await newInquiry.save();
+    res.status(200).json({ success: true, message: 'Inquiry sent successfully!' });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Failed to send inquiry.' });
+  }
+});
+
+app.get("/", (req, res) => res.sendFile(__dirname + '/dist/index.html'))
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
